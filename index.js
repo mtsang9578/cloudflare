b800dbd6ae16558f210a5966e1e819e8a33aab29
mtsang9578 <myaetsang@gmail.com>
2020-04-24 02:30:28 -0400

    added await statements

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 addEventListener('fetch', event => {
-  // let callFetch = new Promise(
-  //   function(resolve, reject) {
-  //     console.log("inside the function")
-  //     resolve(fetch("https://cfw-takehome.developers.workers.dev/api/variants"))
-  //   });
-  // let stuff = fetchData();
-  event.respondWith(fetchData());
+  // event.respondWith(fetchData());
+  event.respondWith(handleRequest(event.request))
 })
 
 function fetchData() {
@@ -15,20 +10,39 @@ function fetchData() {
   .then(data => {
     return new Promise(
       function(resolve, reject) {
-        response = new Response(JSON.stringify(data));
-        resolve(response);
+        urls = data["variants"]
+        index = Math.floor(Math.random() * 2)
+        console.log(urls[index]);
+        res = fetch(urls[index])
+        resolve(res);
     });
   })
   .catch(error => {
     console.log("there was an error", error);
   });
 }
+
+class ElementHandler {
+  element(element) {
+   console.log(`Incoming element: ${element.tagName}`);
+  }
+}
+
+
+
+
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request){
-  return new Response('hello world', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  let response = await fetch("https://cfw-takehome.developers.workers.dev/api/variants");
+  let data = await response.json();
+  let urls = data["variants"];
+  index = Math.floor(Math.random() * 2);
+  return fetch(urls[index]);
+  let newResponse = new HTMLRewriter()
+  .on('*', new ElementHandler())
+  .transform(response);
+  return newResponse;
 }
